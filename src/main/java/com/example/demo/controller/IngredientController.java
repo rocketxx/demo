d6c739a1c2d3dba01e64d227d65551e5636d3327
rocketxx <william97.ragusa@gmail.com>
2024-06-12 12:43:53 +0200

    creazione id casuale

--- src/main/java/com/example/demo/controller/IngredientController.java
+++ src/main/java/com/example/demo/controller/IngredientController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -44,6 +45,7 @@ public class IngredientController {
     
     @PostMapping("/create")
     public Ingredient createIngredient(@RequestBody Ingredient ingredient) {
+        ingredient.setId(UUID.randomUUID().toString());
         return ingredientsRepository.save(ingredient);
     }
 
