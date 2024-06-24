008d08f767d1604961e8304c3858acd9a4622cec
rocketxx <william97.ragusa@gmail.com>
2024-06-24 10:57:26 +0200

    aggiunto controller, reopsitory per order item

---
+++ src/main/java/com/example/demo/controller/OrderItemController.java
@@ -0,0 +1,47 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.OrderItem;
+import com.example.demo.repository.OrderItemRepository;
+
+@RestController
+@RequestMapping("/order-items")
+public class OrderItemController {
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
+    @GetMapping("/get-by-user/{userId}")
+    public List<OrderItem> getByRestaurantId(@PathVariable String userId) {
+        return orderItemRepository.findByUserId(userId);
+    }
+
+    @PostMapping("/create")
+    public OrderItem createOrder(@RequestBody OrderItem order) {
+        return orderItemRepository.save(order);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteOrderItem(@PathVariable String id) throws Exception {
+        Optional<OrderItem> optionalOrderItem = orderItemRepository.findById(id);
+        if (optionalOrderItem.isPresent()) {
+            orderItemRepository.delete(optionalOrderItem.get());
+            return "OrderItem deleted with id " + id;
+        } else {
+            // Handle the case where the OrderItem is not found
+            // This can be customized based on your application's requirements
+            throw new Exception("OrderItem not found with id " + id);
+        }
+    }
+
+}
--- src/main/java/com/example/demo/model/OrderItem.java
+++ src/main/java/com/example/demo/model/OrderItem.java
@@ -6,11 +6,26 @@ import java.util.List;
 public class OrderItem {
 
     private String itemId;
+    private String restaurantId;
+    private String userId;
     private int quantity;                               
     private List<Ingredient> customizations; //qui i panini/pizze custom
     private MenuItem menuItem; //qui i menu standard predefiniti dal ristorante
     // Getters and setters
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
+    }
     public MenuItem getMenuItem() {
         return menuItem;
     }
