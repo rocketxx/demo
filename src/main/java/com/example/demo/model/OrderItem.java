13be468a5fe6d82cddf3ed3412896f836c051ac6
rocketxx <william97.ragusa@gmail.com>
2024-07-11 20:43:33 +0200

    type aggiunto in order item

--- src/main/java/com/example/demo/model/OrderItem.java
+++ src/main/java/com/example/demo/model/OrderItem.java
@@ -14,6 +14,15 @@ public class OrderItem {
     private List<Ingredient> customizations; //qui i panini/pizze custom
     private MenuItem menuItem; //qui i menu standard predefiniti dal ristorante
     private String note;
+    private String type; 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public String getNote() {
         return note;
     }
